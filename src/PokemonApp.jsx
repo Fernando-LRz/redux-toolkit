330d347880ab1d79be0018da9c97b70595ae7424
Fernando LR <fernando.lorenzo1899@gmail.com>
2024-01-21 19:15:04 -0600

    display pokemons

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { getPokemons } from './store/slices/pokemon';
 
 export const PokemonApp = () => {
 
     const dispatch = useDispatch();
+    const { pokemons, isLoading, page } = useSelector(state => state.pokemons);
 
     useEffect(() => {
         dispatch( getPokemons() );
@@ -13,14 +14,24 @@ export const PokemonApp = () => {
 
     return (
         <>
-            <h1>PokemonApp</h1>
+            <h1>Pokemon</h1>
             <hr />
+            <span>Loading: { isLoading ? 'True' : 'False' }</span>
 
             <ul>
-                <li>Hola</li>
-                <li>Hola</li>
-                <li>Hola</li>
+                {
+                    pokemons.map(({ name }) => (
+                        <li key={ name }>{ name }</li>
+                    ))
+                }
             </ul>
+
+            <button 
+                disabled={ isLoading }
+                onClick={ () => dispatch( getPokemons(page + 1) ) }
+            >
+                Next
+            </button>
         </>
     );
 };
--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -5,12 +5,9 @@ export const getPokemons = ( page = 0 ) => {
     return async( dispatch, getState ) => {
         dispatch( startLoadingPokemons() );
 
-        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
-        // const data = await resp.json();
-
         const resp = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10 }`);
-        console.log(resp)
+        const { data } = resp;
 
-        // dispatch( setPokemons({ pokemons: data.results, page: page + 1 }) );
+        dispatch( setPokemons({ pokemons: data.results, page: page + 1 }) );
     };
 };
